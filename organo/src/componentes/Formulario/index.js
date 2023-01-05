b1f29120d30cfa5bdf504ce693acc1e7c9dab665
Rodrigo Basso Lopes <rodblopes@hotmail.com>
2023-01-05 12:03:14 -0300

    Melhorando Formulário

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
@@ -16,19 +17,46 @@ const Formulario = () => {
 
   ]
 
+const [nome, setNome] = useState('')
+const [cargo, setCargo] = useState('')
+const [imagem, setImagem] = useState('')
+const [time, setTime] = useState('')
+
 const aoSalvar = (evento) => {
   evento.preventDefault()
-  console.log('O form foi submetido')
+  console.log('O form foi submetido =>', nome, cargo, imagem, time)
 }
 
   return (
     <section className="formulario">
       <form onSubmit={aoSalvar}>
       <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu Cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa obrigatorio={true} label="Time"itens={times} />
+        <CampoTexto
+        obrigatorio={true}
+        label="Nome"
+        placeholder="Digite seu nome"
+        valor={nome}
+        aoAlterado={valor => setNome(valor)}
+        />
+        <CampoTexto
+        obrigatorio={true}
+        label="Cargo"
+        placeholder="Digite seu Cargo"
+        valor={cargo}
+        aoAlterado={valor => setCargo(valor)}
+        />
+        <CampoTexto
+        label="Imagem"
+        placeholder="Digite o endereço da imagem"
+        valor={imagem}
+        aoAlterado={valor => setImagem(valor)}
+        />
+        <ListaSuspensa
+        obrigatorio={true}
+        label="Time"itens={times}
+        valor={time}
+        aoAlterado={valor => setTime(valor)}
+        />
         <Botao>
           Criar Card
         </Botao>
--- organo/src/componentes/ListaSuspensa/index.js
+++ organo/src/componentes/ListaSuspensa/index.js
@@ -1,11 +1,9 @@
 import './ListaSuspensa.css'
-const ListaSuspensa = (props) => {
-  console.log(props.itens)
-  
+const ListaSuspensa = (props) => {  
   return (
     <div className="lista-suspensa">
-      <label required={props.required}>{props.label}</label>
-      <select>
+      <label>{props.label}</label>
+      <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.value}>
         {props.itens.map(item => <option key={item}>{item}</option>)}
       </select>
     </div>
