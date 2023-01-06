3bdfc9152b0af509f2bcc52bfeb93ef7875e86ab
Rodrigo Basso Lopes <rodblopes@hotmail.com>
2023-01-06 14:53:43 -0300

    Incluindo o rodapé e finalizando o projeto

--- organo/src/App.js
+++ organo/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
+import Rodape from './componentes/Rodape';
 
 function App() {
 
@@ -52,7 +53,6 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario times={times.map(time=>time.nome)} aoColaboradorCadastrado = {colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      
       {times.map(time => <Time
         key={time.nome}
         nome={time.nome}
@@ -60,6 +60,7 @@ function App() {
         corSecundaria={time.corSecundaria}
         colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
         />)}
+        <Rodape/>
     </div>
   );
 }
---
+++ organo/src/componentes/Rodape/Rodape.js
@@ -0,0 +1,26 @@
+import './Rodape.css'
+
+export const Rodape = () => {
+
+  return (
+    <div>
+      <div className="rodape" >
+        <div className="redes">
+          <img src="/imagens/fb.png" alt="Facebook" />
+          <img src="/imagens/tw.png" alt="Twiter" />
+          <img src="/imagens/ig.png" alt="Istagram" />
+        </div>
+        <div className="organo">
+          <img src="/imagens/logo.png" alt="Rodapé da Página Organo" />
+        </div>
+        <div className="desenvolvido">
+         Desenvolvido por <strong>Rodrigo Basso Lopes</strong>
+        </div>
+      </div>
+
+
+
+    </div>
+  )
+
+}
